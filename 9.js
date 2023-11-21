dc24be75a8eb226f8ebedfea321cd7b3129588b8
TetianaZinchenko <ztar1978@gmail.com>
2023-11-21 10:49:51 +0200

    added js tasks

--- 9.js
+++ 9.js
@@ -7,7 +7,20 @@ function task9() {
   // Викликаємо функцію fetchWithError
   // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+
+  function fetchWithError() {
+    const errorMessage = "Помилка при з'єднанні з сервером";
+    return Promise.reject(errorMessage);
+  }
+
+  fetchWithError()
+    .then((result) => {
+      console.log("Виконаний успішно:", result);
+    })
+    .catch((error) => {
+      console.log("Помилка:", error);
+    });
 }
 
-// Викликаємо функцію task10
+// Викликаємо функцію task9
 task9();
